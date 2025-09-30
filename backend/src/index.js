52a505fa9c998f75d9d06ce3eb528641ee9ca7fb
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-09-29 22:33:40 -0400

    adjustments to backend connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,11 +23,11 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.Node_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
